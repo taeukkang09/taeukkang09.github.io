7b423ec5d4da588b35edbfadad1ee49d4620ad06
Taeuk Kang <taeukkang09@gmail.com>
2020-09-26 17:54:57 -0700

    Redesigned website

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-	var app = document.getElementByClass('title-heading');
-
-	var typewriter = new Typewriter(app, {
-		loop: true
-	});
-
-	typewriter.typeString('Hello World!').start();
-};
